b62d1cbf6c4c29015bd76a5334d03725de26ebae
saurabhchaudhary999 <39827870+saurabhchaudhary999@users.noreply.github.com>
2021-09-01 15:52:34 +0530

    Create simplefunction1.kt

---
+++ simplefunction1.kt
@@ -0,0 +1,16 @@
+fun main()
+    {
+        hello()
+        hello()
+        hello()
+    }
+
+    fun hello()
+    {
+        println("Hello Saurabh")
+        bye()
+    }
+   fun bye()
+   {
+       println("Bye Saurabh")
+   }
