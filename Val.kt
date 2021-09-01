3bd25773e4597610931c05d082c06b8200b23d00
saurabhchaudhary999 <39827870+saurabhchaudhary999@users.noreply.github.com>
2021-09-01 13:04:11 +0530

    Create Val.kt

---
+++ Val.kt
@@ -0,0 +1,18 @@
+fun main() {
+ val myname = "Saurabh C" // By Val you can make read only varibale
+  println(myname)
+ println(myname::class) // you will type of that variable
+
+ val surname: String ="Chaudhary"
+ println(surname)
+println(surname::class)
+
+ val age =32
+ println(age)
+
+ val salary:Int= 30_000_000
+ println(salary)
+ println(salary::class)
+
+
+}
