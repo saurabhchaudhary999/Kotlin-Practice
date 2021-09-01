27ca4c6af0b8a872742e1873e3a427b0c9c47597
saurabhchaudhary999 <39827870+saurabhchaudhary999@users.noreply.github.com>
2021-09-01 16:05:20 +0530

    Create Function_In_Function1.kt

---
+++ Function_In_Function1.kt
@@ -0,0 +1,20 @@
+fun main()
+    {
+        hello()
+    }
+fun hello()
+{
+    println("Hello World")
+
+    fun bye()
+    {
+        println("Bye World..!")
+
+        fun hey()
+        {
+            println("Hey There..!")
+        }
+        hey()
+    }
+    bye()
+}
