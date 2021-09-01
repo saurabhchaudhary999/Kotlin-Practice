9da1fb36f5f8b826d7fce77f9fa6270a8ae37f55
saurabhchaudhary999 <39827870+saurabhchaudhary999@users.noreply.github.com>
2021-09-01 13:49:54 +0530

    Create Condistional_IF.kt

---
+++ Condistional_IF.kt
@@ -0,0 +1,20 @@
+ 
+
+fun main() {
+ var mark= 55
+
+ if (mark>60)
+ {
+  println("You passed with first class.!")
+ }
+ else if( mark >50)
+ {
+  println("You pased with second class")
+ }
+ else
+ {
+  println("You Passed")
+ }
+
+}
+
