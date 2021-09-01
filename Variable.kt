d28e785a36b1620af0ef92033edb574e078aedbf
saurabhchaudhary999 <39827870+saurabhchaudhary999@users.noreply.github.com>
2021-09-01 12:57:59 +0530

    Create Variable.kt

---
+++ Variable.kt
@@ -0,0 +1,16 @@
+fun main() {
+ var myname = "Saurabh C"
+  println(myname)
+
+  myname= "John Nash" //with var you can make multiple variables
+  println(myname)
+
+ myname = ""
+ println(myname.isEmpty())
+
+ var age =32
+ println(age)
+
+ age =55
+ println(age)
+}
