6d631157cda58c0b90afe9de45fdae1c123de99b
saurabhchaudhary999 <39827870+saurabhchaudhary999@users.noreply.github.com>
2021-09-01 16:08:47 +0530

    Create Function_In_Function2.kt

---
+++ Function_In_Function2.kt
@@ -0,0 +1,25 @@
+fun main()
+    {
+        hello()
+
+        val mark =50
+
+        fun exam()
+        {
+            if(mark>60)
+            {
+                println("Passed with first class")
+            }
+            else
+            {
+                println("You Failed")
+            }
+        }
+        exam()
+    }
+
+fun hello()
+{
+    println("Hello World")
+
+}
